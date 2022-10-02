b8a86214cbf9095696b81e58b69c1c3f093356ee
Silas Huereca <silashuereca.dev@gmail.com>
2022-10-02 18:58:51 -0500

    refactor: remove unused code

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import "./style.css";
+import "./assets/css/style.css";
 import router from "./router";
 import App from "./App.vue";
 
