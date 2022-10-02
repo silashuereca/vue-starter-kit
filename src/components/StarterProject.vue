c39d1a8d69c900eef01692e2d0ec6b85d1654e20
Silas Huereca <silashuereca.dev@gmail.com>
2022-10-02 10:55:07 -0500

    feat: basic setup of pinia

--- src/components/StarterProject.vue
+++ src/components/StarterProject.vue
@@ -1,16 +1,24 @@
 <template>
   <div>
     <p v-text="message" />
+    <p v-text="doubleValue" />
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, computed } from "vue";
+import { useCounterStore } from "../stores/counter";
 export default defineComponent({
   setup() {
+    const store: any = useCounterStore();
     const message: string = "Vue Starter Project";
+
+    setInterval(() => {
+      store.increment();
+    }, 1000);
     return {
       message,
+      doubleValue: computed(() => store.doubleCount),
     };
   },
 });
@@ -18,6 +26,6 @@ export default defineComponent({
 
 <style scoped>
 p {
-  font-size: 20px;
+  font-size: 40px;
 }
 </style>
